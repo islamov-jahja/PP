0c34cce0c0b22ad145ecfec8753674a8838b3777
yahya <yahyaislamov15@mail.ru>
2019-06-08 17:47:29 +0300

    внес некоторые изменения

--- lab3/lab3/Program.cs
+++ lab3/lab3/Program.cs
@@ -12,7 +12,7 @@ namespace lab3
         static void Main(string[] args)
         {
             ICriticalSection criticalSection = new AutoResetEventCriticalSection();
-            IPICalculator pICalculator = new PICalculator((Convert.ToInt32(args[0])), Convert.ToInt32(args[1]), Convert.ToInt32(args[1]), criticalSection);
+            IPICalculator pICalculator = new PICalculator((Convert.ToInt32(args[0])), Convert.ToInt32(args[2]), Convert.ToInt32(args[1]), criticalSection);// 1) количество итераций 2) spinCount  3)timeout 
             Stopwatch watch = Stopwatch.StartNew();
             double pi = pICalculator.ToCalculate();
             watch.Stop();
